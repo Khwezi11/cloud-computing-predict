d3305fde1f7834b74edaa92d56ba6cd8d23e3482
Khwezi <khwezizama@gmail.com>
2024-05-03 00:19:50 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://ahgqqsz8qe.execute-api.eu-west-1.amazonaws.com/2401FTDE-OLWZAM-CC-predict-lambda",
+                url: "https://ahgqqsz8qe.execute-api.eu-west-1.amazonaws.com/",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
